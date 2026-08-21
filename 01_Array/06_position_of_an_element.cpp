3f3fa817abc9beb08469cbab3fde21454810444c
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-22 01:41:42 +0530

    01_Array

---
+++ 01_Array/06_position_of_an_element.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int arr[100];
+    cout<<"Enter any "<<n<< "numbers:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"Enter target: ";
+    cin>>target;
+    int index=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            index=i;
+            break;
+        }
+    }
+    if(index!=-1){
+        cout<<target<<" found at index "<<index;
+    }
+    else{
+        cout<<target<< "not found";
+    }
+    return 0;
+}
