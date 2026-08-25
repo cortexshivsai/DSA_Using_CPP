b43c351ddaae7e08b9c33ce75d13b2573b7e1857
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-25 20:03:22 +0530

    01_Array

---
+++ 01_Array/09_reverse_an_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int arr[100];
+    cout<<"Enter any "<<n<<" numbers: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int left=0;
+    int right=n-1;
+    while(left<right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+
+        left++;
+        right--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
