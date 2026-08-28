569659cb224a871c88306442b687c5295794d086
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-28 21:39:11 +0530

    01_Array

---
+++ 01_Array/15_array_left_rotation_by_one_position.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int arr[100];
+    cout<<"Enter any "<<n<<" numbers:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int first=arr[0];
+    for(int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=first;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
